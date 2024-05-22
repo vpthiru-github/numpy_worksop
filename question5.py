2719d37f4766b1895d919e646ca1b10c3a8f1032
vpthiru-github <152064995+vpthiru-github@users.noreply.github.com>
2024-05-22 11:08:47 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,6 +1,7 @@
 #write a program to find if the given number is prime no or not
 
 n=int(input("Enter a number : "))
+a=0
 sqrt_n = int(n**0.5) + 1
 for i in range(3, sqrt_n, 2):
     if n % i == 0:
@@ -8,4 +9,4 @@ for i in range(3, sqrt_n, 2):
         a=1
         break
 if not a:
-    print(n,"is not a prime nimber")
+    print(n,"is a prime nimber")
