3435e466b5f5c492ade0a788b8971353eef066e9
vpthiru-github <152064995+vpthiru-github@users.noreply.github.com>
2024-05-22 10:59:41 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,11 @@
 #write a program to find the factorial of a nummber
+
+n=int(input("Enter a number : "))
+s=2
+if n<2:
+    print(1)
+else:
+    for i in range(3,n+1):
+        s*=i
+    print(s)
+    
